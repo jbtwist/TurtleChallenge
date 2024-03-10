8f26fd4f880013286a4f442ee398e3391832ff40
unknown <user.name>
2024-03-10 15:23:01 +0000

    Add IndexOutofRangeException tests

--- UnitTest.cs
+++ UnitTest.cs
@@ -16,10 +16,50 @@ public class UnitTest1
     [TestMethod]
     public void TestMovementShouldFail()
     {
-        Direction ddir = Direction.UP;
-        Turtle dturtle = new (ddir, 0, 0);
-        Board dboard = new (2, 1, dturtle, [], [1, 0]); //Creates Board with 1 height and 2 width, where exit is placed on [1,0]
-        MoveResult dmove = dboard.Move();
-        Assert.AreEqual(dmove, MoveResult.CANT);
-    }    
+        Direction dir = Direction.UP;
+        Turtle turtle = new (dir, 0, 0);
+        Board board = new (2, 1, turtle, [], [1, 0]); //Creates Board with 1 height and 2 width, where exit is placed on [1,0]
+        MoveResult move = board.Move();
+        Assert.AreEqual(move, MoveResult.CANT);
+    }
+    
+    [TestMethod]
+    public void TestPlacingExitOut()
+    {
+        try{
+        Direction dir = Direction.UP;
+        Turtle turtle = new (dir, 0, 0);
+        Board board = new (2, 1, turtle, [], [4, 0]); 
+        } catch (IndexOutOfRangeException) {
+            return;
+        }
+        Assert.Fail("Not thrown exception");        
+    }
+
+    [TestMethod]
+    public void TestPlacingTurtleOut()
+    {
+        try{
+        Direction dir = Direction.UP;
+        Turtle turtle = new (dir, 5, 5);
+        Board board = new (2, 1, turtle, [], [0, 0]); 
+        } catch (IndexOutOfRangeException) {
+            return;
+        }
+        Assert.Fail("Not thrown exception");        
+    }
+
+    [TestMethod]
+    public void TestPlacingMineOut()
+    {
+        try{
+        Direction dir = Direction.UP;
+        Turtle turtle = new (dir, 1, 0);
+        Board board = new (2, 1, turtle, [4,0], [0, 0]); 
+        } catch (IndexOutOfRangeException) {
+            return;
+        }
+        Assert.Fail("Not thrown exception");        
+    }
+
 }
