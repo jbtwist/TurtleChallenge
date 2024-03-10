bfaccf3551b732b41b23a691aac4d392ac775c65
unknown <user.name>
2024-03-10 15:39:20 +0000

    Add exit and mine tests

--- UnitTest.cs
+++ UnitTest.cs
@@ -22,7 +22,27 @@ public class UnitTest1
         MoveResult move = board.Move();
         Assert.AreEqual(move, MoveResult.CANT);
     }
+
+    [TestMethod]
+    public void TestMovementShouldExit()
+    {
+        Direction dir = Direction.UP;
+        Turtle turtle = new (dir, 0, 0);
+        Board board = new (1, 2, turtle, [], [0,1]); //Creates Board with 2 height and 1 width, where exit is placed on [0,1]
+        MoveResult move = board.Move();
+        Assert.AreEqual(move, MoveResult.EXIT);
+    }
     
+    [TestMethod]
+    public void TestMovementShouldMine()
+    {
+        Direction dir = Direction.UP;
+        Turtle turtle = new (dir, 0, 0);
+        Board board = new (1, 3, turtle, [0,1], [0,2]); //Creates Board with 3 height and 1 width, where mine is placed on [0,1]
+        MoveResult move = board.Move();
+        Assert.AreEqual(move, MoveResult.MINE);
+    }
+
     [TestMethod]
     public void TestPlacingExitOut()
     {
@@ -62,4 +82,29 @@ public class UnitTest1
         Assert.Fail("Not thrown exception");        
     }
 
+    [TestMethod]
+    public void TestTurtleOverlapping()
+    {
+        try{
+        Direction dir = Direction.UP;
+        Turtle turtle = new (dir, 0, 0);
+        Board board = new (5, 5, turtle, [], [0, 0]); 
+        } catch (OverlapException) {
+            return;
+        }
+        Assert.Fail("Not thrown exception");        
+    }
+
+    [TestMethod]
+    public void TestMineOverlapping()
+    {
+        try{
+        Direction dir = Direction.UP;
+        Turtle turtle = new (dir, 0, 0);
+        Board board = new (5, 5, turtle, [1,1], [1, 1]); 
+        } catch (OverlapException) {
+            return;
+        }
+        Assert.Fail("Not thrown exception");        
+    }
 }
