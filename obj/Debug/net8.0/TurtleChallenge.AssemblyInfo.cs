566e4b97cdc83da1d8bfb81a8977e304f066db12
Juan Bautista Villarejo Ortiz <jbtwist92@gmail.com>
2024-03-10 15:46:39 +0000

    Add metadata

--- obj/Debug/net8.0/TurtleChallenge.AssemblyInfo.cs
+++ obj/Debug/net8.0/TurtleChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurtleChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f26fd4f880013286a4f442ee398e3391832ff40")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurtleChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurtleChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
